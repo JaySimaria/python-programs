7ff276cf0815734f885c84f2bcd7551137ea2304
JaySimaria <jaysimaria1808@gmail.com>
2022-01-17 10:11:38 +0530

    Practice programs

---
+++ Is Not Operators.py
@@ -0,0 +1,6 @@
+one=[2,3,4,5]
+two=[2,3,4,5]
+if(one is two):
+    print("one and two have same identity")
+else:
+    print("one and two do not have same identity")
---
+++ parameters in function and scoping.py
@@ -0,0 +1,6 @@
+#Parameters
+def my_function(fname):
+    print(fname+"refsnes")
+my_function("Email")
+my_function("Tobias")
+my_function("Linux")
