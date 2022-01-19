3d2346a6a33120ea6eb59bef09b3fab929ad1d15
JaySimaria <jaysimaria1808@gmail.com>
2022-01-19 10:38:20 +0530

    Programs For Practice

---
+++ If....Else Statement.py
@@ -0,0 +1,7 @@
+#19 Jan 2022    
+# Using 'and' in if....else statement
+x=int(input('enter a number:'))
+if x>=1 and x<=10:
+        print("you typed",x,"which is between 1 and 10")
+else:
+    print("you typed",x,"which is below 1 or above 10")
