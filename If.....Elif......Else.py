7fa067f8056f14fda105fc1c9ef3767389fa39af
JaySimaria <jaysimaria1808@gmail.com>
2022-01-20 11:43:14 +0530

    Programs For Practice

---
+++ If.....Elif......Else.py
@@ -0,0 +1,41 @@
+# TO Display A Numeric Digit In Words
+x=int(input('Enter A Digit:'))
+if x==0:
+    print("Zero")
+elif x==1:
+    print("One")
+elif x==2:
+    print("Two")
+elif x==3:
+    print("Three")
+elif x==4:
+    print("Fourth")
+elif x==5:
+    print("Fifth")
+elif x==6:
+    print("Sixth")
+elif x==7:
+    print("Seventh")
+elif x==8:
+    Print("Eight")
+elif x==9:
+    Print("Nineth")#else part not compulsory
+      
+
+
+
+
+
+
+
+
+
+
+#To Know If A Given Number is Zero Or +Ve Or -Ve
+num=-1
+if num==0:
+    print(num, "is zero")
+elif num>0:
+    print(num, "is positive")
+else:
+    print(num, "negative")
