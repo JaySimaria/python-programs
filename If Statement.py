84f60c4e2083db2cb025ea7963b819e6fc3152e7
JaySimaria <jaysimaria1808@gmail.com>
2022-01-20 12:14:54 +0530

    Practice

--- If Statement.py
+++ If Statement.py
@@ -1,4 +1,13 @@
+#19 JAN 2022
 #Understanding If statements
 num=1
-if num==1
-    print("One
+if num==1:
+    print("One")
+    
+    
+str='Yes'
+if str=='Yes':
+    print("Yes")
+    print("This Is What You Said")
+    print("Your Response Is Good")
+    
