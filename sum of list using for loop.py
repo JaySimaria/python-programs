63409d98fbaace6cbc9d0c9139703f9af9c73be1
JaySimaria <jaysimaria1808@gmail.com>
2022-01-22 07:03:11 +0530

    Programs for Practice

---
+++ sum of list using for loop.py
@@ -0,0 +1,8 @@
+#To find sum of list of numbers using for
+#take a list of numbers
+list = [10,20,30,40,50,60,70,80,90,100]
+sum=0 # initially sum is zero
+for i in list:
+    print(i) #display  the element from list
+    sum+=i #add each element to sum
+print('sum= ',sum)    
