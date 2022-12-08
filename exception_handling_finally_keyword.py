8755a8565b9e3bf04fbd6ebff89a12156d398e5d
JS <jaysimaria1808@gmail.com>
2022-12-08 15:10:29 +0530

    practice

---
+++ exception_handling_finally_keyword.py
@@ -0,0 +1,15 @@
+# 8-12-22
+# exception handling finally keyword
+# No exception Exception raised in try block
+try:
+	k = 5//0 # raises divide by zero exception.
+	print(k)
+
+# handles zerodivision exception
+except ZeroDivisionError:
+	print("Can't divide by zero")
+
+finally:
+	# this block is always executed
+	# regardless of exception generation.
+	print('This is always executed')
