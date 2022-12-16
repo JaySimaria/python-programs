0e980659bcbff787f6a3fb4147ed952eea702a38
JS <jaysimaria1808@gmail.com>
2022-12-16 20:10:05 +0530

    practice program

---
+++ .idea/selection_sort_example.py
@@ -0,0 +1,31 @@
+# 16-12-2022
+# selection sort example
+def selection(a):  # Function to implement selection sort
+    for i in range(len(a)):  # Traverse through all array elements
+        small = i  # minimum element in unsorted array
+        for j in range(i + 1, len(a)):
+            if a[small] > a[j]:
+                small = j
+                # Swap the found minimum element with
+        # the first element
+        a[i], a[small] = a[small], a[i]
+
+
+def printArr(a):  # function to print the array
+
+    for i in range(len(a)):
+        print(a[i], end=" ")
+
+
+a = [10, 2, 5, 8, 6, 11, 25, 1]
+print("Before sorting array elements are - ")
+printArr(a)
+selection(a)
+print("\nAfter sorting array elements are - ")
+selection(a)
+printArr(a)
+
+
+
+
+
