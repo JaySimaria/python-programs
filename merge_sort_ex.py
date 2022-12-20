2760300d29f05d0ba958e015f4729f03695de9b6
JS <jaysimaria1808@gmail.com>
2022-12-20 22:31:09 +0530

    practice programs

---
+++ merge_sort_ex.py
@@ -0,0 +1,23 @@
+# 20-12-22
+# merge sort ex
+
+
+def merge_two_sorted_lists(a, b):
+    sorting_list=[]
+    len_a = len(a)
+    len_b = len(b)
+
+    i = j = 0
+
+    while i < len_a and j < len_b:
+        if a[i] <= b[j]:
+            sorting_list.append(a[i])
+            i+=1
+        else:
+            sorting_list.append(b[j])
+            j+=1
+
+    return sorting_list
+if __name__ == '__main__':
+    a = [2,4,6,7.8]
+    b = [1,3,9,5,6]
