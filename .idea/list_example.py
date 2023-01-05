ec1415bee43f68b8c6d260295039dc6972cdc105
JS <jaysimaria1808@gmail.com>
2023-01-05 17:33:30 +0530

    Practice programs

---
+++ .idea/list_example.py
@@ -0,0 +1,9 @@
+# 5-1-22
+#!/usr/bin/python
+
+list = ['Python', 'Machine Learning', 1997, 7886];
+print ("Value available at index 0")
+print (list[0])
+list[2] = 7886;
+print ("New value available at index  1 ")
+print (list[1])
---
+++ .idea/slicing_in_a_list.py
@@ -0,0 +1,14 @@
+# 5-01-23
+# slicing using a list
+# List slicing in Python
+
+my_list = ['I', 'Love', 'Python', 'Programming', 'Language']
+
+# items from index 2 to index 3
+print(my_list[2:4])
+
+# items from index 4 to end
+print(my_list[4:])
+
+# items beginning to end
+print(my_list[:])
