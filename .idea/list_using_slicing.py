18de25155eb3a1557fe7d4125b02bdcc146986fd
JS <jaysimaria1808@gmail.com>
2023-01-06 18:56:41 +0530

    Practice programs

---
+++ .idea/list_using_slicing.py
@@ -0,0 +1,13 @@
+# 6-01-23
+# list using slicing
+list = [1,2,3,4,5,6,7]
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+# Slicing the elements
+print(list[0:6])
+# By default, the index value is 0 so its starts from the 0th element and go for index -1.
+print(list[:])
+print(list[2:5])
+print(list[1:6:2])
---
+++ .idea/python_lists.py
@@ -0,0 +1,12 @@
+# 6-1-23
+# python list
+
+
+# Creating a List with
+# the use of multiple values
+List = ["I ", "Love", "My", "India"]
+
+# accessing a element from the
+# list using index number
+print("Accessing a element from the list")
+print(List[3])
