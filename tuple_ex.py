c0851a81d30d52df1b3cc316a0d707159e608461
JS <jaysimaria1808@gmail.com>
2023-01-08 19:59:50 +0530

    Practice

---
+++ tuple_ex.py
@@ -0,0 +1,12 @@
+# 8-1-22
+# tuple example
+names = ('Jay', 'Anjali', 'Jalpa', 'Yash')
+print(names[0])  # prints 'Jay'
+print(names[1])  # prints 'Anjali'
+print(names[2])  # prints 'Jalpa'
+print(names[3])  # prints 'Yash'
+
+nums = (1, 2, 3, 4, 5)
+print(nums[0])  # prints 1
+print(nums[1])  # prints 2
+print(nums[4])  # prints 5
