e9578d40a49188534a56512c191ad828d867b4f7
JS <jaysimaria1808@gmail.com>
2023-01-09 22:40:18 +0530

    Practice

---
+++ Tuple1.py
@@ -0,0 +1,14 @@
+# 9-1-23
+# tuple program
+# Tuple with integer types
+a = (30, 10, 20)
+# Tuple with string types
+b = ("Suresh", "Rohini", "Trisha", "Hashish")
+# Tuple with mix types
+c = (10, "Never Give up", 20)
+# Empty Tuple
+d = ()
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
+print("d = ", d)
---
+++ tuple_ex1.py
@@ -0,0 +1,11 @@
+# 9-1-22
+# tuple example
+def my_fun():
+    name = 'John'
+    ID = 23567
+    Title = 'Software Engineer'
+    return name, ID, Title
+
+
+employee = my_fun()
+print('Employee detail is:', employee)
