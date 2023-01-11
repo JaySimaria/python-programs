25940aafbe4807d5aa95fb38d2003631cbecacf4
JS <jaysimaria1808@gmail.com>
2023-01-11 19:23:04 +0530

    Practice

---
+++ set1.py
@@ -0,0 +1,8 @@
+# 11-1-23
+# set another program
+Days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+print(Days)
+print(type(Days))
+print("Looping through the set elements ... ")
+for i in Days:
+    print(i)
---
+++ set_ex1.py
@@ -0,0 +1,10 @@
+# 11-01-22
+# set example 1
+months = {"January", "February", "March", "April", "May", "June"}
+print("\nprinting the original set ... ")
+print(months)
+print("\nRemoving some months from the set...")
+months.remove("January")
+months.remove("May")
+print("\nPrinting the modified set...")
+print(months)
