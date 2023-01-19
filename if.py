ce7b0f2b29b8d96ca1ed622846ef6b957d7a7f72
JS <jaysimaria1808@gmail.com>
2023-01-19 20:14:31 +0530

    Practice program

---
+++ if.py
@@ -0,0 +1,9 @@
+# 19-1-23
+# if__else_example1
+x = 5
+y = 3
+
+if x < y:
+    print("x is smaller than y.")
+else:
+    print("x is greater than y.")
---
+++ if__else__example.py
@@ -0,0 +1,11 @@
+# 19-1-2022
+# if__else__example
+a = 5
+b = 6
+
+if a > b:
+    print("First number is the largest")
+elif a < b:
+    print("Second number is the largest")
+else:
+    print("a and b are equal ")
