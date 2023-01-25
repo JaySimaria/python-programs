57dccd4319b082c67ced894668757227eab6a4da
JS <jaysimaria1808@gmail.com>
2023-01-25 21:48:54 +0530

    Practice program

---
+++ nested_if_example2.py
@@ -0,0 +1,12 @@
+# 25-01-22
+n = 9
+if n % 5 == 0:
+    if n % 3 == 0:
+        print("The number is divisible by both 5 and 3")
+    else:
+        print("The number is divisible by both 5 and 3")
+else:
+    if n % 3 == 0:
+        print("The number is divisible by 3 but not 5")
+    else:
+        print("The number is not divisible by 5 and not divisible by 3")
