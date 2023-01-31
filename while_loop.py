96211e0461a38c2ea04544caeea71e238d87d6d3
JS <jaysimaria1808@gmail.com>
2023-01-31 23:28:39 +0530

    Practice program

---
+++ while_loop.py
@@ -0,0 +1,11 @@
+# 31-1-2023
+#  print any 5 numbers using while loop
+
+# initialize the variable
+i = 1
+n = 5
+
+# while loop from i = 1 to 5
+while i <= n:
+    print(i)
+    i = i + 1
---
+++ while_loop_example.py
@@ -0,0 +1,8 @@
+# 31-1-23
+# to print odd number in the range of 1 to 20  while using while loop
+i = 0
+while i <= 20:
+    i += 1
+    if i % 2 == 1:
+        continue
+    print(i)
