bc10ac5e737942fa421588f75d27b83120286cbf
JS <jaysimaria1808@gmail.com>
2023-02-03 01:10:43 +0530

    Practice program

---
+++ reverse_while_loop.py
@@ -0,0 +1,6 @@
+# 2-2-23
+# to print numbers in reverse order
+i = 10
+while i >= 0:
+    print(i, end=' ')
+    i = i - 1
---
+++ while_loop_using_else_statement.py
@@ -0,0 +1,8 @@
+# 2-2-23
+# to check any numbers is less than 10
+value = 0
+while value < 10:
+    print(value, " is smaller than 10")
+    value = value + 1
+else:  # executes once the condition in the while loop turns false
+    print(value, " is equal to  10")
